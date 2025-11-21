decfa445169ce6ee7c5741823810cdf47ee6ab8d
NguyenPhiLong17 <long0789186055@gmail.com>
2025-11-21 13:34:41 +0700

    Fix prime number check logic in 1.2.py

--- Bai1/1.2.py
+++ Bai1/1.2.py
@@ -1,1 +1,16 @@
+n = int(input("Nhập số nguyên dương n: "))
+
+if n == 1:
+    print(n, "không là số nguyên tố")
+else:
+    if n < 4:
+        print(n, "là số nguyên tố")
+    else:
+        i = 2  
+        while i < n and n % i != 0:
+            i += 1
+        if i == n:
+            print(n, "là số nguyên tố")
+        else:
+            print(n, "không là số nguyên tố")
 
