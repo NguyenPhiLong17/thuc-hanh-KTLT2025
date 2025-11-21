7a58b738a5ac213a71c870430f663efb5a8d7644
NguyenPhiLong17 <long0789186055@gmail.com>
2025-11-21 14:30:25 +0700

    Update 1.1.py

--- Bai1/1.1.py
+++ Bai1/1.1.py
@@ -1,7 +1,13 @@
-# Tính diện tích tam giác
-a = float(input("Nhập cạnh đáy a: "))
-h = float(input("Nhập chiều cao h: "))
+BEGIN
+    DECLARE a, h, S AS REAL
 
-S = 0.5 * a * h
-print("Diện tích tam giác là:", S)
+    OUTPUT "Nhap canh day a: "
+    INPUT a
 
+    OUTPUT "Nhap chieu cao h: "
+    INPUT h
+
+    SET S = 0.5 * a * h
+
+    OUTPUT "Dien tich tam giac la: ", S
+END
